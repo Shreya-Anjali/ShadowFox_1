044cedb84a58cd09a25c50fcb515faa9da6bb0c5
Shreya Anjali Bhallamudi <164635278+Shreya-Anjali@users.noreply.github.com>
2025-06-30 15:52:52 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,10 @@
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    target.scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
